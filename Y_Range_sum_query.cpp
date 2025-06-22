5987e66624becc5073534f716670e95cb42fa9bf
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2025-06-22 17:04:02 +0600

    add new file

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    vector<int> v(n);
+    vector<long long> vsum(n,0);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+       if (i==0)
+       {
+       vsum[i]=v[i];
+       }
+       else
+       {
+        vsum[i]=vsum[i-1]+v[i];
+       }
+
+       
+
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<vsum[i]<<endl;
+    // }
+    
+    for (int i = 0; i < q; i++)
+    {
+        int ini, fi;
+        cin >> ini >> fi;
+        ini--;fi--;
+        cout<<vsum[fi]-vsum[ini]<<endl;
+    }
+
+    return 0;
+}
